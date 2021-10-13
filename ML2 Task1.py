888848de9c2d15bf6ac56158a915d1a962f07598
ZakriaRehman962 <zakriarehman9621@gmail.com>
2021-10-13 20:41:33 +0500

    Second Commit

---
+++ ML2 Task1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 13:09:37 2021
+
+@author: MUHAM
+"""
+number = [1,2,3,4,5,6,7,8,9,10]
+
+for i in number:
+    print()
+    for j in number:    
+        print(i*j, end='\t')
+        
